c00df2717afc2e5bef9069bff2243453606a5c3b
Nils Preusker <nils.preusker@movingimage24.de>
2014-03-28 17:18:43 +0100

    integrating keycloak

--- app/scripts/controllers/todoController.js
+++ app/scripts/controllers/todoController.js
@@ -1,8 +1,22 @@
 'use strict';
 
 angular.module('todoApp', ['ngRoute', 'todoApp.services', 'todoApp.filters'])
-    .controller('todoController', ['$scope', '$location', 'TodoService', function ($scope, $location, TodoService) {
+    .controller('todoController', ['$scope', '$location', 'TodoService', 'Keycloak', function ($scope, $location, TodoService, Keycloak) {
         $scope.todos = TodoService.query();
+
+        $scope.logout = function () {
+            Keycloak.then(function (kc) {
+                kc.logout();
+            });
+        };
+
+        Keycloak.then(function (kc) {
+            if (kc.idToken) {
+                $scope.userName = kc.idToken.preferred_username;
+            } else {
+                console.log('No idToken... need to load it!');
+            }
+        });
 
         $scope.addTodo = function () {
             TodoService.save(
@@ -18,7 +32,11 @@ angular.module('todoApp', ['ngRoute', 'todoApp.services', 'todoApp.filters'])
         };
 
         $scope.toggleDone = function (todo) {
-            TodoService.patch(todo);
+            TodoService.patch(todo).then(function() {
+                // nothing to do here
+            }, function () {
+                todo.done = !todo.done;
+            });
         };
 
         $scope.clearDone = function () {
--- app/scripts/services/todoService.js
+++ app/scripts/services/todoService.js
@@ -8,4 +8,65 @@ service.factory('TodoService', ['$resource', function ($resource) {
             method: 'PATCH'
         }
     });
+}]).factory('Keycloak', ['$q', function ($q) {
+    var deferred = $q.defer();
+    var keycloak = Keycloak({
+        clientId: 'todo-ui',
+        realm: 'demo',
+        onload: 'login-required',
+        url: 'http://localhost',
+        clientSecret: '42814939-2e46-4dbd-ad1e-29aa95f02428'
+    });
+
+    keycloak.init(function () {
+        console.log("Initialization of keycloak succeeded!");
+
+        deferred.resolve(keycloak);
+
+    }, function () {
+        console.log("Initialization of keycloak failed!");
+
+        deferred.reject(keycloak);
+
+    });
+
+    return deferred.promise;
+}]).factory('OAuthInterceptor', ['$q', 'Keycloak', function ($q, keycloak) {
+    return {
+        'request': function (config) {
+
+            var deferred = $q.defer();
+
+            keycloak.then(function (kc) {
+
+                kc.onValidAccessToken(function () {
+
+                        config.headers.Authorization = 'Bearer ' + kc.token;
+
+                        deferred.resolve(config || $q.when(config));
+                    },
+                    function (data) {
+                        console.log("oops, something went wrong!");
+
+                        deferred.reject(data);
+
+
+
+
+//                        kc.login();
+                    });
+            });
+
+            return deferred.promise;
+        },
+        'requestError': function (rejection) {
+
+            console.log("error interceptor called");
+
+            return $q.reject(rejection);
+        }
+    };
+}]).config(['$httpProvider', function ($httpProvider) {
+    $httpProvider.interceptors.push('OAuthInterceptor');
 }]);
+
